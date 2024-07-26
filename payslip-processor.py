794604fea11caf043145444355004cfc2a5525df
Joseph Higaki <josephhigaki@hotmail.com>
2024-07-26 17:02:50 +0200

    ignore pwd, remove temp dir

--- payslip-processor.py
+++ payslip-processor.py
@@ -3,10 +3,6 @@ import shutil
 import zipfile
 import yaml
 
-SOURCE_DIRECTORY = r'C:\Users\Joseph_Higaki\Documents\Nominas'
-TARGET_DIRECTORY = r"C:\Users\Joseph_Higaki\Documents\Nominas\Extracted"
-
-
 def load_paths() -> tuple[str, str]:
     with open("config.yml", "r", encoding="utf-8") as ymlfile:  
         _cfg = yaml.safe_load(ymlfile)
@@ -25,18 +21,20 @@ if __name__ == "__main__":
         temp_dir = Path(target_directory).joinpath(zip_path.name)
         # temp_dir = C:\Users\Joseph_Higaki\Documents\Nominas\Extracted\2022-02.ZIP
         Path(temp_dir).mkdir()
-        with zipfile.ZipFile(zip_path, 'r') as zip_ref:
-            zip_ref.setpassword(zip_pwd.encode('utf-8'))
-            zip_ref.extractall(temp_dir)            
-            for index, pdf_path in enumerate(Path(temp_dir).glob('*.pdf')):
-                # pdf_path = C:\Users\Joseph_Higaki\Documents\Nominas\Extracted\2022-02.ZIP\xxxxx.PDF
-                # Define the new PDF file path (rename or simply move)
-                # new_pdf_path = C:\Users\Joseph_Higaki\Documents\Nominas\Extracted\2022-02.PDF
-                multiple_pdf_files_in_zip = '' if index == 0 else f'_{index}'
-                # if 0 ""
-                # if > 0 "_1"
-                new_pdf_path = Path(target_directory).joinpath(f'{zip_path.stem}{multiple_pdf_files_in_zip}.pdf')
-                # Move the PDF file to the target directory with the new name
-                shutil.move(str(pdf_path), str(new_pdf_path))
-        # Clean up the temporary extraction directory
-        shutil.rmtree(temp_dir)
+        try: 
+            with zipfile.ZipFile(zip_path, 'r') as zip_ref:
+                zip_ref.setpassword(zip_pwd.encode('utf-8'))
+                zip_ref.extractall(temp_dir)            
+                for index, pdf_path in enumerate(Path(temp_dir).glob('*.pdf')):
+                    # pdf_path = C:\Users\Joseph_Higaki\Documents\Nominas\Extracted\2022-02.ZIP\xxxxx.PDF
+                    # Define the new PDF file path (rename or simply move)
+                    # new_pdf_path = C:\Users\Joseph_Higaki\Documents\Nominas\Extracted\2022-02.PDF
+                    multiple_pdf_files_in_zip = '' if index == 0 else f'_{index}'
+                    # if 0 ""
+                    # if > 0 "_1"
+                    new_pdf_path = Path(target_directory).joinpath(f'{zip_path.stem}{multiple_pdf_files_in_zip}.pdf')
+                    # Move the PDF file to the target directory with the new name
+                    shutil.move(str(pdf_path), str(new_pdf_path))
+        finally:
+            # Clean up the temporary extraction directory
+            shutil.rmtree(temp_dir)
